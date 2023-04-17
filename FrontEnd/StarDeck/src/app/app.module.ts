02446d093a0216f23eadee9285626509897090aa
Axel Cordero <axeljcm@estudiantec.cr>
2023-04-16 18:15:52 -0600

    componentes login y crear carta

--- FrontEnd/StarDeck/src/app/app.module.ts
+++ FrontEnd/StarDeck/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CrearCartaComponent } from './crear-carta/crear-carta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CrearCartaComponent
   ],
   imports: [
     BrowserModule,
---
+++ FrontEnd/StarDeck/src/app/crear-carta/crear-carta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-carta',
+  templateUrl: './crear-carta.component.html',
+  styleUrls: ['./crear-carta.component.css']
+})
+export class CrearCartaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
