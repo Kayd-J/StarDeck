c5de74a5c0e5862d6649cf96005c2124dc2bc962
Roy <roychav99@gmail.com>
2023-04-21 00:07:16 -0600

    Formulario basico

--- FrontEnd/StarDeck/src/app/app-routing.module.ts
+++ FrontEnd/StarDeck/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearCartaComponent } from './crear-carta/crear-carta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'crearCarta', component: CrearCartaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
