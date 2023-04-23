2e749e0e706dbc95e0d459c32af1820958749aa3
Mario <48070830+mjcarranza@users.noreply.github.com>
2023-04-23 09:59:33 -0600

    Creación de API
    API para jugadores y cartas

---
+++ StarDeckAPI/StarDeckAPI/Models/Carta.cs
@@ -0,0 +1,13 @@
+﻿namespace StarDeckAPI.Models
+{
+    public class Carta
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Tipo { get; set; } = string.Empty;   
+        public string Raza { get; set; } = string.Empty;
+        public int Energia { get; set; } 
+        public int Costo{ get; set; }
+        public bool Disponibilidad { get; set; }
+    }
+}
---
+++ StarDeckAPI/StarDeckAPI/Models/Jugador.cs
@@ -0,0 +1,23 @@
+﻿namespace StarDeckAPI.Models
+{
+    public class Jugador
+    {
+        public int ID { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public string Apellido { get; set; } = string.Empty;
+
+        public string Usuario { get; set; } = string.Empty;
+
+        public int País { get; set; }
+
+        public string Avatar { get; set; } = string.Empty;
+
+        public string Correo { get; set; } = string.Empty;
+
+        public string Contraseña { get; set; } = string.Empty;
+
+        public int Estado { get; set; }
+    }
+}
