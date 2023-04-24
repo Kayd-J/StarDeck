c68d5f1a10c246ef1e37be8922b125e6e8dccef7
Axel Cordero <axeljcm@estudiantec.cr>
2023-04-24 15:01:26 -0600

    cambios scripts sql

--- BasedeDatos/GenerarTablas.sql
+++ BasedeDatos/GenerarTablas.sql
@@ -6,17 +6,27 @@ CREATE TABLE JUGADOR(
 	apellidos VARCHAR(30) not null,
 	nacionalidad VARCHAR(20) not null,
 	correo VARCHAR(40) NOT NULL,
-	estadodecuenta bit not null
+	estadodecuenta bit not null,
+	idpais int not null
 );
 CREATE TABLE CARTA(
 	idcarta int not null,
 	energia int not null,
 	raza VARCHAR(20) not null,
 	costo int not null,
-	nombrecarta VARCHAR(40) not null,
+	nombre VARCHAR(40) not null,
 	tipo VARCHAR(20) not null,
 	disponibilidad bit not null,
+	idadmin int not null
 );
+CREATE TABLE ADMINISTRADOR(
+	idadmin int not null,
+	nombre VARCHAR(20) not null,
+	apellido VARCHAR(30) not null,
+	correo VARCHAR(40) not null,
+	pass VARCHAR(20) not null
+);
+
 
 CREATE TABLE CARTA_JUGADOR(
 	idcarta int not null,
@@ -24,17 +34,36 @@ CREATE TABLE CARTA_JUGADOR(
 	cantidad int not null
 );
 
+CREATE TABLE PAISES(
+	idpais int not null,
+	pais VARCHAR(30) not null
+);
+
+
 ALTER TABLE JUGADOR
 ADD PRIMARY KEY(idjugador);
 
 ALTER TABLE CARTA
 ADD PRIMARY KEY(idcarta);
 
+ALTER TABLE JUGADOR
+ADD PRIMARY KEY(idadmin);
+
+ALTER TABLE PAISES
+ADD PRIMARY KEY(idpais)
+
+ALTER TABLE JUGADOR
+ADD FOREIGN KEY (idpais) REFERENCES PAISES(idpais);
+
+ALTER TABLE CARTA
+ADD FOREIGN KEY (idadmin) REFERENCES ADMINISTRADOR(idadmin);
+
 ALTER TABLE CARTA_JUGADOR
 ADD FOREIGN KEY (idcarta) REFERENCES CARTA(idcarta);
 
 ALTER TABLE CARTA_JUGADOR
 ADD FOREIGN KEY (idjugador) REFERENCES JUGADOR(idjugador);
+
 
 
 
