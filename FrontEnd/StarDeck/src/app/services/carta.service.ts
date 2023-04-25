2b60c0d228e8aeb1a9470a1bc45487e28e5d02dd
Roy <roychav99@gmail.com>
2023-04-25 00:51:43 -0600

    Service carta para post
    Error en import enviroment

--- FrontEnd/StarDeck/src/app/services/carta.service.ts
+++ FrontEnd/StarDeck/src/app/services/carta.service.ts
@@ -1,19 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
+import { environment } from 'src/environments/environment';
+import { Carta } from '../models/carta';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartaService {
-  private url = "crearCarta";
-  apiurl = "";
-
-
+  private url = "crear-carta";
+  
   constructor(private http: HttpClient) {}
 
-  addCarta(inputdata:any){
-    return this.http.post(this.apiurl,inputdata);
+  public addCarta(carta: Carta): Observable<Carta[]>{
+    return this.http.post<Carta[]>(
+      `${environment.apiURL}/${this.url}`, carta
+    );
   }
 
+  public updateCarta(carta: Carta): Observable<Carta[]>{
+    return this.http.put<Carta[]>(
+      `${environment.apiURL}/${this.url}`, carta
+    );
+  }
 }
