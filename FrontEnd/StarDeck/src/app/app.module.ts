656d85e80e660e19ee8a1356785673ee0797b3e8
Roy <roychav99@gmail.com>
2023-05-05 22:55:26 -0600

    Update crear deck
    Se agregaron los componentes principales para crear deck

--- FrontEnd/StarDeck/src/app/app.module.ts
+++ FrontEnd/StarDeck/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,7 +30,8 @@ import { CrearDeckComponent } from './crear-deck/crear-deck.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CarouselModule.forRoot()
   ],
   exports:[
     LoginComponent,
