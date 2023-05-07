5c9f0d7810c4e244d5e3c6e27fad97cb5e9c9aa5
Axel Cordero <axeljcm@estudiantec.cr>
2023-05-07 00:19:29 -0600

    Actualizacion de back end

---
+++ StarDeckAPI/StarDeckAPI/StarDeckAPI/Models/CartaJugador.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace StarDeckAPI.Models;
+
+public class CartaJugador
+{
+    public int Cantidad { get; set; }
+    public string CartaId { get; set; }
+    public string JugadorId { get; set; }
+    
+
+}
---
+++ StarDeckAPI/StarDeckAPI/StarDeckAPI/Models/Jugador.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace StarDeckAPI.Models;
+
+public class Jugador
+{
+    public string Id { get; set; }
+
+    public string Usuario { get; set; } = null!;
+
+    public string Pass { get; set; } = null!;
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellidos { get; set; } = null!;
+
+    public string Nacionalidad { get; set; } = null!;
+
+    public string Correo { get; set; } = null!;
+
+    public bool Estadodecuenta { get; set; }
+
+    public int Avatar { get; set; }
+
+    public string paisesId { get; set; } = null!;
+
+    // public Paises paises { get; set; } = null!;
+
+    // public List<Carta> cartas { get; set; } = null!;
+}
