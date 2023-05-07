22a94738c424dd37d8290c2f58c29a7df458ca08
Roy <roychav99@gmail.com>
2023-05-06 22:35:23 -0600

    modelos y servicios

---
+++ FrontEnd/StarDeck/src/app/models/planeta.ts
@@ -0,0 +1,11 @@
+export class Planeta{
+    idplaneta!: number;
+    nombre!: string;
+    descripcion!: string;
+    imagen!: string;
+    idadmin!: number;
+    disponiblidad!: boolean;
+    habilidad!: string;
+    tipo!: number;
+  }
+  
