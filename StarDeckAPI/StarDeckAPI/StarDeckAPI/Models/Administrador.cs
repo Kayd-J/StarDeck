4ede846aed6a891e504e1ec49ce0017145c2d404
Axel Cordero <axeljcm@estudiantec.cr>
2023-05-06 22:19:07 -0600

    Falto unos modelos de actualizar

---
+++ StarDeckAPI/StarDeckAPI/StarDeckAPI/Models/Administrador.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace StarDeckAPI.Models;
+
+public class Administrador
+{
+    public string Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public string Correo { get; set; } = null!;
+
+    public string Pass { get; set; } = null!;
+
+    // public List<Carta> Cartas { get; set; }
+}
