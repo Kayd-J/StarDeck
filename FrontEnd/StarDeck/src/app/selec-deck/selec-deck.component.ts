f5c3d36c6a073ff34e4827f9d8d2b3e223ebae21
Roy <roychav99@gmail.com>
2023-06-08 19:10:25 -0600

    Agregado visualizar deck

--- FrontEnd/StarDeck/src/app/selec-deck/selec-deck.component.ts
+++ FrontEnd/StarDeck/src/app/selec-deck/selec-deck.component.ts
@@ -4,10 +4,14 @@ import { ApiService } from '../services/api.service';
 import { MatchmakingComponent } from '../matchmaking/matchmaking.component';
 import { DeckVariableService } from '../services/deck-variable.service';
 import { CookieService } from 'ngx-cookie-service';
+import { CarouselConfig } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-selec-deck',
   templateUrl: './selec-deck.component.html',
+  providers: [
+    { provide: CarouselConfig, useValue: { interval: 0, noPause: true, showIndicators: true } }
+ ],
   styleUrls: ['./selec-deck.component.css']
 })
 
@@ -18,6 +22,16 @@ export class SelecDeckComponent implements OnInit {
 
   listDecks!: deck[];
   deck:any;
+  itemsPerSlide = 6;
+  singleSlideOffset = true;
+  slides = [
+    {image: 'assets/Images/Barglo.jpg'},
+    {image: 'assets/Images/Kepto.jpg'},
+    {image: 'assets/Images/Mazli.jpg'},
+    {image: 'assets/Images/Yeka.jpg'},
+    {image: 'assets/Images/Warloc.jpg'},
+    {image: 'assets/Images/Lumien.jpg'},
+ ];
 
   constructor(private api:ApiService,private deckvar: DeckVariableService, private cookieService: CookieService ) { }
 
